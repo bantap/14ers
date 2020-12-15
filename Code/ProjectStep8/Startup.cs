a9fae0f71edfd8346f197761f2bd64f8b9e3a17e
Paul Banta <bantap@erau.edu>
2020-12-14 19:52:21 -0700

    Published. Environment Connection String

--- Code/ProjectStep8/Startup.cs
+++ Code/ProjectStep8/Startup.cs
@@ -28,12 +28,16 @@ namespace ProjectStep8
       // This method gets called by the runtime. Use this method to add services to the container.
       public void ConfigureServices(IServiceCollection services)
       {
-         services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer
-               (Configuration.GetConnectionString("DefaultConnection")));
+         // services.AddDbContext<AppDbContext>(options =>
+         //    options.UseSqlServer
+         //       (Configuration.GetConnectionString("DefaultConnection")));
          // services.AddDbContext<AppDbContext>(options =>
          //    options.UseSqlServer
          //       (Configuration.GetConnectionString("AzureConnection")));
+
+         services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer
+               (System.Environment.GetEnvironmentVariable("14ersConnectionString")));
 
          services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
